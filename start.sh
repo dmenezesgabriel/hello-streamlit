7dec43cfdf29189e5f1da9e99c3323025f5035bd
Gabriel Menezes <dmenezes.gabriel@gmail.com>
2023-08-25 07:59:04 -0300

    Update start.sh script

--- start.sh
+++ start.sh
@@ -1,2 +1,41 @@
+#!/bin/bash
+
+# Define variables
 ENV=DEVELOPMENT
-streamlit run app/src/Dashboard.py
+PYTHON_EXECUTABLE=""
+DASHBOARD_FILE="app/src/Dashboard.py"
+REQUIREMENTS_FILE="app/requirements.txt"
+
+# Define functions
+set_python_executable() {
+  if [ "$(uname -s)" = "Linux" ]; then
+    PYTHON_EXECUTABLE=venv/bin/python
+  elif [ "$(uname -s)" = "Windows" ]; then
+    PYTHON_EXECUTABLE=venv/Scripts/python
+  elif [ "$(uname -s)" = "Darwin" ]; then
+    PYTHON_EXECUTABLE=venv/bin/python
+  else
+    echo "Unsupported operating system"
+    exit 1
+  fi
+}
+
+create_virtual_environment() {
+  if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+    echo "Installing requirements..."
+    $PYTHON_EXECUTABLE -m pip install -r $REQUIREMENTS_FILE
+  fi
+}
+
+start_dashboard() {
+  echo "Starting Streamlit Dashboard..."
+  echo "Environment: $ENV"
+  $PYTHON_EXECUTABLE -m streamlit run $DASHBOARD_FILE
+}
+
+# Main script
+set_python_executable
+create_virtual_environment
+start_dashboard
